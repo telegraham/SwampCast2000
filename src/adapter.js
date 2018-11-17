227055fb28d4590e0c368efef3bf31b94128f635
Graham Troyer-Joy <graham.troyer-joy@flatironschool.com>
2018-11-16 22:11:58 -0500

    heroku url

--- src/adapter.js
+++ src/adapter.js
@@ -1,5 +1,5 @@
-const API_ROOT = 'http://localhost:3000';
-export const API_WS_ROOT = 'ws://localhost:3000/cable';
+const API_ROOT = 'https://tempapi-backend.herokuapp.com';
+export const API_WS_ROOT = 'ws://tempapi-backend.herokuapp.com/cable';
 // const HEADERS = {
 //   'Content-Type': 'application/json',
 //   Accept: 'application/json',
