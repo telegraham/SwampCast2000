f986b0c9149e339084fdc2f2391f6fef159312ef
Graham Troyer-Joy <graham.troyer-joy@flatironschool.com>
2018-12-17 00:12:26 -0500

    build

---
+++ docs/precache-manifest.d9244ce31ba54140384692f0f0ddafb3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "33cc99b2f24d7a8b943f",
+    "url": "/SwampCast2000/static/js/runtime~main.33cc99b2.js"
+  },
+  {
+    "revision": "655260e47a7e14fbbaa1",
+    "url": "/SwampCast2000/static/js/main.655260e4.chunk.js"
+  },
+  {
+    "revision": "52f86b5266084b2b1049",
+    "url": "/SwampCast2000/static/js/1.52f86b52.chunk.js"
+  },
+  {
+    "revision": "655260e47a7e14fbbaa1",
+    "url": "/SwampCast2000/static/css/main.5427fb2e.chunk.css"
+  },
+  {
+    "revision": "876b3b13860ba3db2c4612a16c2c236a",
+    "url": "/SwampCast2000/index.html"
+  }
+];
